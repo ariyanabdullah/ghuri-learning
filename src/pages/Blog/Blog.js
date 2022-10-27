36ce82d8598ebc43e1d4102e44172fd4852eb2cf
ariyanabdullah <ak01859136696@gmail.com>
2022-10-27 06:36:51 +0600

    first question box style added

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -4,14 +4,16 @@ const Blog = () => {
   return (
     <section>
       {/* 1st question */}
-      <div>
-        <h3> What is The cors in Node Js ?</h3>
-        <p>
-          = CORS stands for Cross-Origin Resource Sharing . It allows us to
-          relax the security applied to an API. This is done by bypassing the
-          Access-Control-Allow-Origin headers, which specify which origins can
-          access the API.
-        </p>
+      <div className="mx-auto w-75">
+        <div className="mx-auto my-5 shadow-lg rounded py-3 ps-2 pe-2">
+          <h3>1. What is The cors in Node Js ?</h3>
+          <p className="lead">
+            = CORS stands for Cross-Origin Resource Sharing . It allows us to
+            relax the security applied to an API. <br /> This is done by
+            bypassing the Access-Control-Allow-Origin headers, which specify
+            which origins can access the API.
+          </p>
+        </div>
       </div>
     </section>
   );
