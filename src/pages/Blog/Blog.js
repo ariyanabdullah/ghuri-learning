e4000af034f98659cbd3eb5ca9780d570999914f
ariyanabdullah <ak01859136696@gmail.com>
2022-10-27 06:32:26 +0600

    first answer added

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -6,6 +6,12 @@ const Blog = () => {
       {/* 1st question */}
       <div>
         <h3> What is The cors in Node Js ?</h3>
+        <p>
+          = CORS stands for Cross-Origin Resource Sharing . It allows us to
+          relax the security applied to an API. This is done by bypassing the
+          Access-Control-Allow-Origin headers, which specify which origins can
+          access the API.
+        </p>
       </div>
     </section>
   );
