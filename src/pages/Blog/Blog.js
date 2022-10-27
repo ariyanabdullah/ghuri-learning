5ca7cf6d6323ca681ada581a601bd7522295503c
ariyanabdullah <ak01859136696@gmail.com>
2022-10-27 06:40:14 +0600

    seconde question added

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -3,8 +3,8 @@ import React from "react";
 const Blog = () => {
   return (
     <section>
-      {/* 1st question */}
       <div className="mx-auto w-75">
+        {/* 1st question */}
         <div className="mx-auto my-5 shadow-lg rounded py-3 ps-2 pe-2">
           <h3>1. What is The cors in Node Js ?</h3>
           <p className="lead">
@@ -13,6 +13,13 @@ const Blog = () => {
             bypassing the Access-Control-Allow-Origin headers, which specify
             which origins can access the API.
           </p>
+        </div>
+        {/* second question */}
+        <div className="">
+          <h3>
+            2. Why are you using firebase? What other options do you have to
+            implement authentication?
+          </h3>
         </div>
       </div>
     </section>
